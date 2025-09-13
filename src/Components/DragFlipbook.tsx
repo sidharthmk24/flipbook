83a9fa873eaff1b03869189ecccf876a6fa3388f
sidharthmk24 <sidharthmk24@gmail.com>
2025-09-13 09:37:12 +0530

    second commit

--- src/Components/DragFlipbook.tsx
+++ src/Components/DragFlipbook.tsx
@@ -7,9 +7,13 @@ export default function Iframe(){
     <>
     
 <div style={{ position: "relative", width: "100%", height: "700px" }}>
-   <iframe style="position:absolute;border:none;width:100%;height:100%;left:0;top:0;" src="https://online.fliphtml5.com/mmind3266/ggte/"  seamless="seamless" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true" ></iframe>
-    </div>    </>
+<iframe
+  className="w-full md:w-full h-[600px] md:h-[700px] shadow-lg rounded-md"
+  src="https://online.fliphtml5.com/mmind3266/ggte/"
+  allowTransparency
+  allowFullScreen
+></iframe>
+  </div>    </>
   )
 }
 
-// <div style="position:relative;padding-top:max(60%,324px);width:100%;height:0;"></div>
--- src/Components/Iframe/Iframe.tsx
+++ src/Components/Iframe/Iframe.tsx
@@ -4,7 +4,12 @@ import React from "react";
 export default function Iframe() {
     return (
   <div className="flex justify-center items-center w-full py-10 bg-none">
-  <iframe   className="w-[100%] md:w-[100%] h-[600px] md:h-[700px] shadow-lg rounded-md" src="https://online.fliphtml5.com/mmind3266/ggte/"  seamless="seamless" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true" ></iframe>
-  </div>
+<iframe
+  className="w-full md:w-full h-[600px] md:h-[700px] shadow-lg rounded-md"
+  src="https://online.fliphtml5.com/mmind3266/ggte/"
+  frameBorder="0"
+  allowTransparency
+  allowFullScreen
+></iframe>  </div>
     );
 }
