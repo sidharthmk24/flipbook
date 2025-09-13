3d8e5c6a21118e4615935e79206bdbee0f83a3ea
sidharthmk24 <sidharthmk24@gmail.com>
2025-09-13 11:29:08 +0530

    last

--- src/app/flipbook/page.tsx
+++ src/app/flipbook/page.tsx
@@ -10,7 +10,7 @@ const Page = () => {
     <>
     <Iframe/>
     
-    <div className="flex justify-center items-center w-full min-h-screen">
+    <div className="flex justify-center items-center w-full min-h-screen bg-black">
 
 
       <StageFlip
